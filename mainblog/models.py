0a9768bbe2e171f0b4c716bc3b5eb8c8b28e3830
pfigueroa03 <70380500+pfigueroa03@users.noreply.github.com>
2022-04-08 17:00:55 -0300

    Translate project create form

--- mainblog/models.py
+++ mainblog/models.py
@@ -5,13 +5,13 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Project(models.Model):
-    project_name = models.CharField(max_length=50)
-    subtitle = models.CharField(max_length=50)
-    investigation_area = models.CharField(max_length=50)
-    summary = models.CharField(max_length=75)
+    project_name = models.CharField(max_length=50, verbose_name = "Titulo")
+    subtitle = models.CharField(max_length=50, verbose_name = "Sub-Titulo")
+    investigation_area = models.CharField(max_length=50, verbose_name = "Area de trabajo")
+    summary = models.CharField(max_length=75, verbose_name = "Resumen")
     autor = models.CharField(max_length=50)
     images = models.ImageField(verbose_name = "Imagen", upload_to="mainblog")
-    description = RichTextField(blank=True, null=True)
+    description = RichTextField(blank=True, null=True, verbose_name = "Descripcion")
     created = models.DateTimeField(auto_now_add = True, verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now = True, verbose_name="Fecha de edicion")
 
