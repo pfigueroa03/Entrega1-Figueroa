262cdf269570d69090682183078e17362cc5372b
pfigueroa03 <70380500+pfigueroa03@users.noreply.github.com>
2022-04-17 10:57:53 -0300

    Fix image resize, delete unused codelines, small visual improve in project description

--- mainblog/forms.py
+++ mainblog/forms.py
@@ -1,18 +1,1 @@
 from django import forms
-
-"""class ProyectoFormulario(forms.Form):
-    project_name = forms.CharField()
-    investigation_area = forms.CharField()
-    description = forms.CharField(widget=forms.Textarea())
-
-
-class AutorForm(forms.Form):
-    firstName = forms.CharField()
-    lastName = forms.CharField()
-    country = forms.CharField()
-
-
-class PublishingCompanyForm(forms.Form):
-    publishing_name = forms.CharField()
-    location = forms.CharField()
-    publication_date = forms.DateField()"""
